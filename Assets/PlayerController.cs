17e5d43fd1929e3d236b078b1c5bb44ea650632c
Ritvik Durgempudi <rintu1121@outlook.com>
2024-05-11 19:46:50 -0700

    Added background and respawn

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 respawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "FallDetector"){
+            //what will happen when player enters fall detector zone
+            transform.position = respawnPoint;
+        }
+    }
+}
