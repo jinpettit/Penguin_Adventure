b745927a56d0ddbbb59c541763255b9427c6c2c2
Jinrong Pettit <jinpettit@gmail.com>
2024-05-15 03:19:47 -0700

    combat and animation

--- Assets/EnemyPatrol.cs
+++ Assets/EnemyPatrol.cs
@@ -10,6 +10,9 @@ public class EnemyPatrol : MonoBehaviour
     private Animator anim;
     private Transform currentPoint;
     public float speed;
+
+    public int maxHealth = 1;
+    private int currentHealth;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,29 +20,39 @@ public class EnemyPatrol : MonoBehaviour
         anim = GetComponent<Animator>();
         currentPoint = pointB.transform;
         anim.SetBool("isMoving", true);
-
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 point = currentPoint.position - transform.position;
-        if(currentPoint == pointB.transform){
-            rb.velocity = new Vector2(speed, 0) ;
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
         }
-        else{
+        else
+        {
             rb.velocity = new Vector2(-speed, 0);
         }
 
-        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform){
-           currentPoint = pointA.transform;
-           
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+
         }
 
-        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform){
-           currentPoint = pointB.transform;
-           
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
- 
--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -10,7 +10,7 @@ using UnityEngine.SceneManagement;
 public class PlayerInput : MonoBehaviour
 {
     private float horizontal;
-    
+
     private float speed = 2f;
     private float jumpingPower = 4f;
     private bool isFacingRight = true;
@@ -18,31 +18,38 @@ public class PlayerInput : MonoBehaviour
 
     public AudioSource audioPlayer;
 
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+    private Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
 
     public BoxCollider2D regCol;
     public BoxCollider2D slideCol;
     public Vector3 respawnPoint;
-
+    public bool isGrounded;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
 
     void Start()
     {
         //regCol.enabled = true;
         //slideCol.enabled = false;
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
         respawnPoint = transform.position;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        // isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(.05f, .02f), CapsuleDirection2D.Horizontal, 0, groundLayer);
 
-        if (Input.GetButtonDown("Jump"))
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
@@ -54,9 +61,10 @@ public class PlayerInput : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            anim.SetBool("IsAttacking", true);
+            Attack();
+
         }
-        
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             anim.SetBool("IsSlide", true);
@@ -64,11 +72,39 @@ public class PlayerInput : MonoBehaviour
             //anim.SetBool("IsSlide", false);
         }
 
-
         Flip();
     }
 
-    private void Slide(){
+    void Attack()
+    {
+        anim.SetBool("IsAttacking", true);
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            StartCoroutine(DisableEnemyWithDelay(enemy.gameObject));
+        }
+    }
+
+    IEnumerator DisableEnemyWithDelay(GameObject enemy)
+    {
+        yield return new WaitForSeconds(0.25f);
+        enemy.SetActive(false);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    private void Slide()
+    {
         //regCol.enabled = false;
         //slideCol.enabled = true;
         speed = 5f;
@@ -106,28 +142,36 @@ public class PlayerInput : MonoBehaviour
         anim.SetBool("IsAttacking", false);
     }
 
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.tag == "FallDetector"){
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "FallDetector")
+        {
             //what will happen when player enters fall detector zone
             transform.position = respawnPoint;
         }
-        else if(other.tag == "Checkpoint"){
+        else if (other.tag == "Checkpoint")
+        {
             //what will happen when player enters checkpoint zone
             respawnPoint = other.transform.position;
         }
-        else if(other.tag == "Ice"){
+        else if (other.tag == "Ice")
+        {
             //what will happen when player enters ice zone
             speed = 3f;
         }
-        else if(other.tag == "Normal"){
+        else if (other.tag == "Normal")
+        {
             //what will happen when player enters normal zone
             speed = 2f;
         }
-        else if (other.tag == "Seal" && anim.GetBool("IsAttacking") == false){
+        else if (other.tag == "Seal" && anim.GetBool("IsAttacking") == false)
+        {
             transform.position = respawnPoint;
         }
-        else if(other.tag == "Final"){
+        else if (other.tag == "Final")
+        {
             SceneManager.LoadScene(2);
         }
     }
+
 }
